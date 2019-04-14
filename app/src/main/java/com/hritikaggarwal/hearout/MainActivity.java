91a58f0744306f1fc0a5144c42b1f9ec6cdb7f0f
Pratinava Dhamija <pratinavadhamija@gmail.com>
2019-04-13 19:41:17 -0700

    Notifcation Code

--- app/src/main/java/com/hritikaggarwal/hearout/MainActivity.java
+++ app/src/main/java/com/hritikaggarwal/hearout/MainActivity.java
@@ -1,13 +1,47 @@
 package com.hritikaggarwal.hearout;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
     }
+
+    public void sendOnChannel1(View v) {
+        String title = "Somebody's calling you!";
+        String message = "Tap to Hear them out";
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_hearout)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+
+
+        notificationManager.notify(1, notification);
+    }
+
 }
