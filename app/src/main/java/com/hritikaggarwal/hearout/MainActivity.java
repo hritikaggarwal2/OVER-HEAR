553e6ae8dfd1039953439febbcc90c2fb6b5e10d
varkon256 <kondavarsha@hotmail.com>
2019-04-13 18:23:55 -0700

    Vibrator code

--- app/src/main/java/com/hritikaggarwal/hearout/MainActivity.java
+++ app/src/main/java/com/hritikaggarwal/hearout/MainActivity.java
@@ -2,12 +2,35 @@ package com.hritikaggarwal.hearout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.Button;
+import android.view.View;
+import android.os.VibrationEffect;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    Vibrator vibrator;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.vibrate(VibrationEffect.createOneShot(10000, 255));
+            }
+        });
+
+        button2 = findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.cancel();
+            }
+        });
     }
 }
