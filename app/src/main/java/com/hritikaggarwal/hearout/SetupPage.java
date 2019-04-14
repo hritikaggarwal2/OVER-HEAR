539498db6ffb41c610bfc0890372ffb52fd20ce6
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2019-04-13 21:31:28 -0700

    Subtle Changes

--- app/src/main/java/com/hritikaggarwal/hearout/SetupPage.java
+++ app/src/main/java/com/hritikaggarwal/hearout/SetupPage.java
@@ -5,13 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import java.io.FileOutputStream;
 
 public class SetupPage extends AppCompatActivity {
 
@@ -20,7 +16,7 @@ public class SetupPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_page);
 
-        Button btn = (Button) findViewById(R.id.button2);
+        Button btn = (Button) findViewById(R.id.start_using);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
